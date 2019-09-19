19cc6dce8facadbb9a721231d142037a493dbedd
CyberEgor <ceres042@yahoo.com>
2019-09-19 17:04:29 +0700

    Updates

---
+++ precache-manifest.7a11c02600a7a2f8f2963dca3141c953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2537e4835086a2606547ef37850c296c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55f3df506542aa4dcc80",
+    "url": "./static/css/2.be9fd68d.chunk.css"
+  },
+  {
+    "revision": "1d15c18f1216771da635",
+    "url": "./static/css/main.141dd8e2.chunk.css"
+  },
+  {
+    "revision": "55f3df506542aa4dcc80",
+    "url": "./static/js/2.91620c36.chunk.js"
+  },
+  {
+    "revision": "1d15c18f1216771da635",
+    "url": "./static/js/main.c8a02f6a.chunk.js"
+  },
+  {
+    "revision": "cc9468b5b683a46d6e73",
+    "url": "./static/js/runtime~main.8cfa84b2.js"
+  },
+  {
+    "revision": "c61a6ac9c0156199f0341f6b0ec7f4ab",
+    "url": "./static/media/default_img.c61a6ac9.PNG"
+  }
+]);
